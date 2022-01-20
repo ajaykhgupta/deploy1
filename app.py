e38c8adad036d77d7fc05841df07e9ab602c37c6
Ajay K Gupta <ajay.k.gupta@innovaccer.com>
2022-01-20 17:40:49 +0530

    added

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
 db = SQLAlchemy(app)
 
 class Item(db.Model):
